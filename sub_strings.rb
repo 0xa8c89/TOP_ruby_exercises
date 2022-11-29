3cd94d4caf8201f2bcc31a20a235eed2d8c532a4
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-29 21:11:31 +0100

    Do sub strings part 2

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,14 +1,16 @@
-def substrings(word, array)
+def substrings(words, array)
     array.reduce(Hash.new(0)) do |hash, i|
-        if i == word
-            hash[word] += 1
-        elsif word.include?(i)
-            hash[i] += 1
+        words.split(' ').each do |word|
+            if i == word
+                hash[word] += 1
+            elsif word.include?(i)
+                hash[i] += 1
+            end
         end
         hash
     end
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
