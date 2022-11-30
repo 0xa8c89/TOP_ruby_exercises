cb440d2419b3630e9583df5f35d696c7ee0d2362
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-30 01:12:03 +0100

    Do bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+def bubble_sort(array)
+    previous = Array.new
+
+    for i in 1..array.length-1
+        for i in 0..array.length-2
+            if array[i] > array[i+1]
+                array[i], array[i+1] = array[i+1], array[i]
+            end
+        end
+    end
+    array
+end
+
+p bubble_sort([9,6,5,3,2])
