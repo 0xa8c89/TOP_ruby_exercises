b0c4fc936112ec2eeac5792b760a30e6c4a1efa6
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2023-01-14 01:33:00 +0100

    refactor code

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,25 +1,13 @@
 def caesar_cipher(string, shift)
-    result = String.new
-    alphabet = String.new  # cesar alphabet
+  result = ''
+  alphabet = ''  # cesar alphabet
 
-    for i in 0..((shift/26.0).ceil)
-        for i in 'a'..'z'
-            alphabet << i
-        end
-    end
-    for i in 0..((shift/26.0).ceil)
-        for i in 'A'..'Z'
-            alphabet << i
-        end
-    end
+  (0..((shift / 26.0).ceil)).each { ('a'..'z').each { |i| alphabet << i } }
+  (0..((shift / 26.0).ceil)).each { ('A'..'Z').each { |i| alphabet << i } }
 
-    string.each_char do |char|
-        if alphabet.include?(char)
-            result << alphabet[alphabet.index(char) + shift]
-        else
-            result << char
-        end
-    end
-    result
+  string.each_char do |char|
+    alphabet.include?(char) ? result << alphabet[alphabet.index(char) + shift] : result.push(char)
+  end
+  result
 end
-puts caesar_cipher('What a string!', 5)
+# puts caesar_cipher('What a string!', 5)
